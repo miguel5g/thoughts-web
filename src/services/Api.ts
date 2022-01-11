ccce2ee67f0d58d328ab8ea01bff4647213b6fd8
Miguel <miguelcg958@gmail.com>
2022-01-11 12:59:48 -0300

    Decoupled urls from api

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const isProduction = process.env.NODE_ENV === 'production';
+const prodURL = 'https://thoughts-5g.herokuapp.com/api';
+const devURL = 'http://localhost:4000/api';
 
-export const api = axios.create({
-  baseURL: isProduction ? 'https://thoughts-5g.herokuapp.com/api' : 'http://localhost:4000/api',
-});
+export const api = axios.create({ baseURL: isProduction ? prodURL : devURL });
