4a250f114f3ce6a013ab04d8757832abe737ea04
Miguel <miguelcg958@gmail.com>
2022-01-12 12:37:08 -0300

    Added some meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,44 @@ class AppDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          {/* Primary Meta Tags */}
+          <meta
+            name="title"
+            content="Pensamentos do Miguel - Um site onde eu posto o que penso e o que quero!"
+          />
+          <meta
+            name="description"
+            content="Um simples site para publicar meus pensamentos e notas mentais sem me preocupar com o que os outros iram pensar, pois é meu local de anotações públicas!"
+          />
+          <meta name="theme-color" content="#121214" />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://thoughts-sigma.vercel.app/" />
+          <meta
+            property="og:title"
+            content="Pensamentos do Miguel - Um site onde eu posto o que penso e o que quero!"
+          />
+          <meta
+            property="og:description"
+            content="Um simples site para publicar meus pensamentos e notas mentais sem me preocupar com o que os outros iram pensar, pois é meu local de anotações públicas!"
+          />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary" />
+          <meta property="twitter:url" content="https://thoughts-sigma.vercel.app/" />
+          <meta
+            property="twitter:title"
+            content="Pensamentos do Miguel - Um site onde eu posto o que penso e o que quero!"
+          />
+          <meta
+            property="twitter:description"
+            content="Um simples site para publicar meus pensamentos e notas mentais sem me preocupar com o que os outros iram pensar, pois é meu local de anotações públicas!"
+          />
+
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
