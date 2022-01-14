4b6989f9ebbe244fee01361866c8d17e7d292bb8
Miguel <miguelcg958@gmail.com>
2022-01-14 14:38:43 -0300

    Removed get static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import { GetStaticProps } from 'next';
 
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
@@ -7,22 +6,14 @@ import { Post } from '../components/Post';
 import { Button } from '../components/Button';
 import { MonthlyHeader } from '../components/MonthlyHeader';
 
-import { getPosts } from '../utils/GetPosts';
 import { usePosts } from '../hooks/usePosts';
 
-interface HomeProps {
-  totalPages: number;
-  posts: PostData[];
-}
-
-const Home = ({ posts: initialPosts }: HomeProps) => {
+const Home = () => {
   const { handleLoadMore, isLastPage, isLoading, posts } = usePosts();
 
   function isFirstOfMonthly(index: number): boolean {
-    const postList = posts.length > 0 ? posts : initialPosts;
-
-    const current = postList[index];
-    const previous = postList[index - 1];
+    const current = posts[index];
+    const previous = posts[index - 1];
 
     if (!previous) return true;
 
@@ -40,36 +31,31 @@ const Home = ({ posts: initialPosts }: HomeProps) => {
 
       <Header />
 
-      <main className="flex flex-col flex-1 gap-3 mx-6">
-        <ul className="flex flex-col max-w-3xl gap-3 mx-auto">
-          {(posts.length > 0 ? posts : initialPosts).map((post, index) => (
-            <li key={post.id}>
-              {isFirstOfMonthly(index) && <MonthlyHeader date={post.createdAt} />}
+      {posts.length <= 0 && isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <main className="flex flex-col flex-1 gap-3 mx-6">
+          <ul className="flex flex-col max-w-3xl gap-3 mx-auto">
+            {posts.map((post, index) => (
+              <li key={post.id}>
+                {isFirstOfMonthly(index) && <MonthlyHeader date={post.createdAt} />}
 
-              <Post post={post} />
-            </li>
-          ))}
-        </ul>
+                <Post post={post} />
+              </li>
+            ))}
+          </ul>
 
-        {!isLastPage && (
-          <Button className="mx-auto mt-4" onClick={handleLoadMore}>
-            {isLoading ? 'Carregando...' : 'Carregar mais'}
-          </Button>
-        )}
-      </main>
+          {!isLastPage && (
+            <Button className="mx-auto mt-4" onClick={handleLoadMore}>
+              {isLoading ? 'Carregando...' : 'Carregar mais'}
+            </Button>
+          )}
+        </main>
+      )}
 
       <Footer />
     </div>
   );
-};
-
-export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-  const data = await getPosts();
-
-  return {
-    props: data,
-    revalidate: 15,
-  };
 };
 
 export default Home;
