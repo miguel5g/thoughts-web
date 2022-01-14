902550e741fb486445cc59496c953e39cfd09ae2
Miguel <miguelcg958@gmail.com>
2022-01-14 14:22:14 -0300

    Fixed import details

--- src/contexts/PostsContext.tsx
+++ src/contexts/PostsContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from 'react';
+
 import { getPosts } from '../utils/GetPosts';
 
 interface PostsContextType {
--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+
 import { PostsContext } from '../contexts/PostsContext';
 
 export function usePosts() {
