aada2887ac11ffaf6bfc8374f855eb97b6af8a79
Miguel <miguelcg958@gmail.com>
2022-01-15 23:40:24 -0300

    Updated class order

--- src/components/skeleton/MonthlyHeaderSkeleton.tsx
+++ src/components/skeleton/MonthlyHeaderSkeleton.tsx
@@ -1,10 +1,10 @@
 export const MonthlyHeaderSkeleton = () => {
   return (
-    <div className="relative my-3 p-2">
-      <div className="top-1/2 left-0 -translate-y-1/2 absolute w-full bg-gray-highlight h-px" />
-      <div className="top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 absolute w-64 bg-gray h-3" />
-      <div className="animate-pulse flex items-center justify-center">
-        <div className="h-3 w-48 bg-gray-highlight rounded-full"></div>
+    <div className="relative p-2 my-3">
+      <div className="absolute left-0 w-full h-px -translate-y-1/2 top-1/2 bg-gray-highlight" />
+      <div className="absolute w-64 h-3 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 bg-gray" />
+      <div className="flex items-center justify-center animate-pulse">
+        <div className="w-48 h-3 rounded-full bg-gray-highlight"></div>
       </div>
     </div>
   );
