98268cf717066d9b51e5ccf64d2e51f351099476
Miguel <miguelcg958@gmail.com>
2022-01-16 01:29:32 -0300

    Created search box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { MonthlyHeader } from '../components/MonthlyHeader';
 import { MonthlyHeaderSkeleton, PostSkeleton } from '../components/skeleton';
 
 import { usePosts } from '../hooks/usePosts';
+import { SearchBox } from '../components/SearchBox';
 
 const Home = () => {
   const { handleLoadMore, isLastPage, isLoading, posts } = usePosts();
@@ -33,36 +34,48 @@ const Home = () => {
       <Header />
 
       <main className="flex flex-col flex-1 gap-3 mx-6">
-        <ul className="flex flex-col w-full max-w-3xl gap-3 mx-auto">
-          {isLoading && posts.length === 0 ? (
-            <>
-              <li>
-                <MonthlyHeaderSkeleton />
-                <PostSkeleton />
-              </li>
-              <li>
-                <PostSkeleton />
-              </li>
-              <li>
-                <PostSkeleton />
-              </li>
-            </>
-          ) : (
-            posts.map((post, index) => (
-              <li key={post.id}>
-                {isFirstOfMonthly(index) && <MonthlyHeader date={post.createdAt} />}
+        <div className="flex flex-col w-full max-w-3xl gap-6 mx-auto">
+          <SearchBox />
 
-                <Post post={post} />
+          <ul className="flex flex-col w-full gap-3">
+            {isLoading && posts.length === 0 ? (
+              <>
+                <li>
+                  <MonthlyHeaderSkeleton />
+                  <PostSkeleton />
+                </li>
+                <li>
+                  <PostSkeleton />
+                </li>
+                <li>
+                  <PostSkeleton />
+                </li>
+              </>
+            ) : (
+              posts.map((post, index) => (
+                <li key={post.id}>
+                  {isFirstOfMonthly(index) && <MonthlyHeader date={post.createdAt} />}
+
+                  <Post post={post} />
+                </li>
+              ))
+            )}
+
+            {!isLoading && posts.length === 0 && (
+              <li className="flex flex-col items-center justify-center p-6">
+                <img src="/svg/void.svg" alt="Vazio" className="w-2/5 mb-6" />
+                <h2 className="text-3xl font-bold text-white">Não encontrei nada com este termo</h2>
+                <p className="text-sm">Tente procurar por outras palavras ou frases!</p>
               </li>
-            ))
+            )}
+          </ul>
+
+          {!isLastPage && posts.length > 0 && (
+            <Button className="mx-auto mt-4" onClick={handleLoadMore}>
+              {isLoading ? 'Carregando...' : 'Carregar mais'}
+            </Button>
           )}
-        </ul>
-
-        {!isLastPage && posts.length > 0 && (
-          <Button className="mx-auto mt-4" onClick={handleLoadMore}>
-            {isLoading ? 'Carregando...' : 'Carregar mais'}
-          </Button>
-        )}
+        </div>
       </main>
 
       <Footer />
